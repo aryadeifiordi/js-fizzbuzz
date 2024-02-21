53866ee18963727a646bce243a77d274e4b5f48a
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-21 16:13:36 +0100

    ciclo + insertAdjacentHTML

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -9,5 +9,26 @@ const listElement = document.querySelector('.lista');
 // creo ciclo
 
 for(let i = 1; i <= 100; i++){
-    
+    // se i numeri sono multipli di 3, di 5, o di entrambi e stampo il corrispettivo output
+  if (i % 3 == 0 && i % 5 == 0) {
+
+    //console.log(fizzbuzz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${fizzbuzz}</li>`);
+
+  } else if (i % 3 == 0) {
+
+    //console.log(fizz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${fizz}</li>`);
+
+  } else if (i % 5 == 0) {
+
+    //console.log(buzz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${buzz}</li>`);
+
+  } else {
+
+    // console.log(i)
+    listElement.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
+
+  };
 }
