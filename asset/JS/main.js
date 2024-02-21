de36688187b04129f886849017c0023c95b84b92
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-21 16:04:52 +0100

    costanti

---
+++ asset/JS/main.js
@@ -0,0 +1,13 @@
+// creo le costanti 
+const fizz = 'Fizz';
+const buzz = 'Buzz';
+const fizzbuzz = 'FizzBuzz'
+const listElement = document.querySelector('.lista');
+
+//console.log(listElement);
+
+// creo ciclo
+
+for(let i = 1; i <= 100; i++){
+    
+}
